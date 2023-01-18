b0496c89237ec60043c08e75e84304014a74061a
Carlos Herrera <herreradiaz14@gmail.com>
2023-01-18 00:31:50 -0500

    Desafio numero 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
+import { ContentPlaceComponent } from './components/content-place/content-place.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LeftMenuComponent,
+    ContentPlaceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/content-place/content-place.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-place',
+  templateUrl: './content-place.component.html',
+  styleUrls: ['./content-place.component.css']
+})
+export class ContentPlaceComponent {
+
+}
